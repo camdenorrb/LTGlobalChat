f8157f1b9c8f5f3c70f92307891df0b2730d0662
camdenorrb <5360339+camdenorrb@users.noreply.github.com>
2022-05-28 05:24:09 -0500

    Move to Kotlin gradle

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+	java
+	id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "me.camdenorrb"
+version = "1.1.0"
+
+repositories {
+	mavenCentral()
+	maven("https://hub.spigotmc.org/nexus/content/repositories/public/") {
+		name = "SpigotMC"
+	}
+}
+
+dependencies {
+	compileOnly("org.spigotmc:spigot-api:1.18.2-R0.1-SNAPSHOT")
+}
+
+tasks {
+	compileJava {
+		sourceCompatibility = "1.8"
+		targetCompatibility = "1.8"
+	}
+}
